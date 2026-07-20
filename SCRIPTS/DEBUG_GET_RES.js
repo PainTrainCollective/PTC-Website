845f17a543c68574df5b89b1abef62d50aea827a
Myappule <collectorspaintrain@gmail.com>
2026-07-20 04:00:13 -0600

    I learned a lot. Faced Problems and fixed them.
    I had a problem where in when using an
    <iframe> to a another HTML file containing a
    redirect, it would redirect within the <iframe>
    
    This would cause the page you were trying to
    redirect to, load in the iframe and not the
    Top level tab.
    
    I thought it was over until I found out about the
    "Sandbox" attribute and its properties.
    allow-scripts
    and
    allow-top-navigation-by-user-activation
    
    These two things fixed the problem.
    
    I couldn't have found this out without
    https://developer.mozilla.org/en-US/docs/Web/HTML
    /Reference/Elements/iframe#browser_compatibility
    
    Also added a font and a fonts folder.

--- SCRIPTS/DEBUG_GET_RES.js
+++ SCRIPTS/DEBUG_GET_RES.js
@@ -1,6 +1,8 @@
 var width = window.innerWidth;
 var hight = window.innerHeight;
 
-function GetResolution() {
+function GetRes() {
     console.log('The resolution is: Width: ' + width + ", Height: " + hight)
 }
+
+// This is really useless, but it sometimes is needed.
